3724d4d39195b37fd1912ec0c95ba5dd68dd8aa7
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-12-02 10:45:14 -0600

    Update main.py
    Was terrible code. I'm ashamed I ever made that crap.

--- main.py
+++ main.py
@@ -1,68 +1,67 @@
-import praw, time, config, random, difflib, requests
+import time, praw, yaml, random, hackernews, traceback, difflib
 
-reddit = praw.Reddit(client_id=config.cid, client_secret=config.cs, user_agent=config.ua, password=config.p, username=config.u)
-subreddit = reddit.subreddit(config.sub)
+with open("config.yaml") as config_file:
+    config = yaml.safe_load(config_file)
+    client_id = config["client_id"]
+    client_secret = config["client_secret"]
+    username = config["username"]
+    password = config["password"]
+    user_agent = config["user_agent"]
+    usernames = config["usernames"]
+    subs = config["sub_list"]
+    python = config["python"]
+    linux = config["linux"]
+    programming = config["programming"]
+    politics = config["politics"]
+    internet = config["internet"]
+    gaming = config["gaming"]
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     password=password,
+                     username=username)
+
 reddit.validate_on_submit = True
-x = random.randint(0,2)
+x = random.randint(0,1)
 
 while True:
     try:
-        for submission in subreddit.stream.submissions(skip_existing=True):
-            for results in subreddit.search(submission.title):
+        for submission in reddit.subreddit(subs).stream.submissions(skip_existing=True):
+            for results in reddit.subreddit(subs).search(submission.title):
                 similarity = difflib.SequenceMatcher(None, submission.title,results.title).ratio()
-                if results.num_comments >= 3 and similarity >= .8:
+                if results.num_comments >= 2 and similarity >= .8:
                     comment = results.comments[x]
-                    if comment.author not in config.usernames and comment.body != '[deleted]':
+                    if comment.author not in usernames and comment.body != '[deleted]':
                         submission.reply(comment.body)
                         break
-                        
-        link = 'https://hacker-news.firebaseio.com/v0/newstories.json'
-        stories = json.loads(requests.get(link).text)
-        s = str(stories[0])
-        story = json.loads(requests.get('https://hacker-news.firebaseio.com/v0/item/' + s + '.json').text)
-        title = story['title']
-        url = story['url']
-        normalized_title = title.lower()
-        if 'hn' not in normalized_title:
-            for word in config.programming:
-                with open("database.txt", "r") as db:
-                    if word in normalized_title and url not in db.read():
-                        submit = reddit.subreddit("programming").submit(title, url=url)
-                        with open("database.txt", "a") as f:
-                            f.write(url + "\n")
-                            time.sleep(600)
-            for word in config.internet:
-                with open("database.txt", "r") as db:
-                    if word in normalized_title and url not in db.read():
-                        submit = reddit.subreddit("internetisbeautiful").submit(title, url=url)
-                        with open("database.txt", "a") as f:
-                            f.write(url + "\n")
-                            time.sleep(600)
-                            
-            for word in config.politics:
-                with open("database.txt", "r") as db:
-                    if word in normalized_title and url not in db.read():
-                        submit = reddit.subreddit("politics").submit(title, url=url)
-                        with open("database.txt", "a") as f:
-                            f.write(url + "\n")
-                            time.sleep(600)
-            for word in config.gaming:
-                with open("database.txt", "r") as db:
-                    if word in normalized_title and url not in db.read():
-                        submit = reddit.subreddit("gaming").submit(title, url=url)
-                        with open("database.txt", "a") as f:
-                            f.write(url + "\n")
-                            time.sleep(600)
-                            
-            for word in config.science:
-                with open("database.txt", "r") as db:
-                    if word in normalized_title and url not in db.read():
-                        submit = reddit.subreddit("science").submit(title, url=url)
-                        with open("database.txt", "a") as f:
-                            f.write(url + "\n")
-                            time.sleep(600)
-                                                
-    except Exception:
-        with open("errors.txt", "a") as f:
-            f.write(str(traceback.format_exc()) + "\n")
-            time.sleep(60)
+
+        new_stories = hackernews.HackerNews().new_stories(limit=1)
+        item_id = str(new_stories).replace('[<hackernews.Item: ','').split(" -",1)[0]
+        item = hackernews.HackerNews().get_item(item_id)
+        title = item.title.lower()
+        url = item.url
+        print(title,url)
+        with open('db.txt') as db:
+            if url not in db.read():
+                def get_sub():
+                    for word in python:
+                        if word in title: return 'python'; break
+                    for word in linux:
+                        if word in title: return 'linux'; break
+                    for word in programming:
+                        if word in title: return 'programming'; break
+                    for word in politics:
+                        if word in title: return 'politics'; break
+                    for word in internet:
+                        if word in title: return 'internet'; break
+                    for word in gaming:
+                        if word in title: return 'gaming'; break
+                if get_sub() is not None:
+                    reddit.submit(get_sub, title, url)
+                else: print('no sub found')
+                with open('db.txt', 'a') as f: f.write(url + '\n')
+            else: print('url is in database')
+        time.sleep(60)
+    except Exception: print(str(traceback.format_exc()));time.sleep(60)
+    except KeyboardInterrupt: print('shutting down :(');quit()
