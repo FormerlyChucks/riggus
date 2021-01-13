0c643a4414cdbf92ac3b83ce1407c254422d44da
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-01-13 12:37:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,32 @@ with open("config.yaml") as config_file:
     internet = config["internet"]
     gaming = config["gaming"]
 
+def get_sub():
+    for word in python:
+        if word in title:
+            return 'python'
+            break
+    for word in linux:
+        if word in title:
+            return 'linux'
+            break
+    for word in programming:
+        if word in title:
+            return 'programming'
+            break
+    for word in politics:
+        if word in title:
+            return 'politics'
+            break
+    for word in internet:
+        if word in title:
+            return 'internet'
+            break
+    for word in gaming:
+        if word in title:
+            return 'gaming'
+            break
+
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent,
@@ -44,24 +70,19 @@ while True:
         print(title,url)
         with open('db.txt') as db:
             if url not in db.read():
-                def get_sub():
-                    for word in python:
-                        if word in title: return 'python'; break
-                    for word in linux:
-                        if word in title: return 'linux'; break
-                    for word in programming:
-                        if word in title: return 'programming'; break
-                    for word in politics:
-                        if word in title: return 'politics'; break
-                    for word in internet:
-                        if word in title: return 'internet'; break
-                    for word in gaming:
-                        if word in title: return 'gaming'; break
-                if get_sub() is not None:
-                    reddit.submit(get_sub, title, url)
-                else: print('no sub found')
-                with open('db.txt', 'a') as f: f.write(url + '\n')
-            else: print('url is in database')
+                continue
+            if get_sub() is not None:
+                reddit.submit(get_sub(), title, url)
+                else:
+                    print('no sub found')
+                with open('db.txt', 'a') as f:
+                    f.write(url + '\n')
+            else:
+                print('url is in database')
         time.sleep(60)
-    except Exception: print(str(traceback.format_exc()));time.sleep(60)
-    except KeyboardInterrupt: print('shutting down :(');quit()
+    except Exception:
+        print(traceback.format_exc())
+        time.sleep(60)
+    except KeyboardInterrupt:
+        print('shutting down :(')
+        quit()
