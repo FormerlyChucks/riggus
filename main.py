945aa7c9932eec6e256a8695b8e9471161b7bb2d
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-04-07 08:48:59 -0500

    HN == NO_UPVOTES

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ reddit_secret = ''
 reddit_agent = ''
 
 #lists
-linux = ['debian', 'linus', 'linux', 'redhat', 'torvalds', 'ubuntu']
-programming = ['guido van rossum', 'pycon', 'python', '.py', 'bash', 'c++', 'code', 'coding', 'emacs lisp', 'git', 'golang', 'graphql', 'javascript', 'jquery', 'json', 'jupyter notebook', 'laravel', 'malware', 'npm', 'operating system', 'open source', 'php', 'programming', 'rss', 'software', 'sql', 'windows', 'yaml', '.js']
-politics = ['ballot', 'biden', 'democrat', 'gop', 'government', 'melania', 'pelosi', 'politics', 'president', 'republican', 'sanders', 'senate', 'senator', 'trump']
-internet = ['artificial intelligence', 'facebook', 'google', 'instagram', 'internet', 'reddit', 'snapchat', 'twitter']
-gaming = ['call of duty', 'gaming', 'minecraft', 'nintendo', 'playstation', 'ps3', 'ps4', 'video game', 'wii', 'xbox']
-censorship = ['censor']
 rssguilds = ['politics', 'news', 'conservative']
 feeds = ['https://www.thegatewaypundit.com/feed/', 'https://bigleaguepolitics.com/feed/', 'https://www.dailywire.com/feeds/rss.xml']
 guild_list = ['dankmemes', 'memes', '4chan', 'tumblrinaction', 'okbuddyretard', 'averageredditor']
@@ -42,21 +36,6 @@ def reddit2ruqqus(sub_guild):
     p = {'board': sub_guild,'title': title,'url': url}
     post = ruqqus.post('/api/v1/submit',data=p)
     return 'success'
-
-def get_guild(title):
-    for word in linux:
-        if word in title: return 'linux'
-    for word in programming:
-        if word in title: return 'programming'
-    for word in politics:
-        if word in title: return 'politics'
-    for word in internet:
-        if word in title: return 'internet'
-    for word in gaming:
-        if word in title: return 'gaming'
-    for word in censorship:
-        if word in title: return 'censorship'
-    return 'internet'
 
 while True:
     try:
@@ -87,25 +66,6 @@ while True:
             gl = random.choice(guild_list)
             post = reddit2ruqqus(sub_guild=gl)
             print('SUBMITTED TO +{} | ({})'.format(gl,'REDDIT'))
-        #get links from hn to whore that tasty rep
-        hn_feed = 'https://hnrss.org/newest'
-        for entry in feedparser.parse(hn_feed).entries:
-            hntext = '{} {}'.format(entry.link,entry.title)
-            with open('hnstuff','a') as hn:
-                hn.write(hntext + "\n")
-        else:
-            with open('hnstuff') as hn:
-                hnlines = [l.rstrip() for l in hn]
-                randhn = random.choice(hnlines)
-                hnlink = randhn.split()[0]
-                hntitle = randhn.replace(hnlink,'')
-                gg = get_guild(title=hntitle)
-                hnp = {'board': gg,
-                       'title': hntitle,
-                       'url': hnlink}
-                ruqqus.post('/api/v1/submit', data=hnp)
-                print('SUBMITTED TO +{} | ({})'.format(gg,'RSS FEEDS'))
-            open('hnstuff','r+').truncate(0)
         time.sleep(60)
     except Exception:
         print(traceback.format_exc())
